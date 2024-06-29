41431facd82b25b1b9422a820e12d50dcc7fcb3b
cbhnhi <cbhnhi47@gmail.com>
2024-06-29 19:17:49 +0300

    cleaning up before NodeJS

---
+++ JS/webStorage.js
@@ -0,0 +1,12 @@
+//web storage API
+
+//not part of DOM- its part of the window API
+//done through the variable window
+//window is always assumed to be the object used unless specified otherwise
+
+
+//same result
+window.alert("hi");
+alert("hi");
+
+//sessionStorage
